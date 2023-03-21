d5bce2d541dc447c0c46305d6b7c2a139faa3d55
hboday <97432007+hboday@users.noreply.github.com>
2023-03-21 08:21:40 +0300

    Delete db directory

--- db/migrate/20230309091805_add_manager_id_to_employees.rb
+++
@@ -1,5 +0,0 @@
-class AddManagerIdToEmployees < ActiveRecord::Migration[7.0]
-  def change
-    add_column :employees, :manager_id, :integer
-  end
-end
--- db/migrate/20230310111019_add_gravatar_email_to_employees.rb
+++
@@ -1,5 +0,0 @@
-class AddGravatarEmailToEmployees < ActiveRecord::Migration[7.0]
-  def change
-    add_column :employees, :gravatar_email, :string
-  end
-end
