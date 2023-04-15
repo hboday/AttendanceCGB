9251b62a36925af5fc0881cede016ff58906654e
Hessa Boday <hboday@ub20-srv2.qatar.cmu.edu>
2023-04-15 21:40:30 +0300

    notifications linked to attendance logs

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -5,13 +5,29 @@ export default class extends Controller {
 
   static targets = ["slidebar", "toggleButton"];
   static classes = ["open"];
+  static values = { sidebarUserId: Number };
 
+  connect() {
+    this.slidebarTarget.addEventListener('click', () => {
+      this.toggle();
+    });
+  }
 
+  disconnect() {
+    this.slidebarTarget.removeEventListener('click', () => {
+      this.toggle();
+    });
+  }
 
   toggle() {
     this.slidebarTarget.classList.toggle(this.openClass);
-    const text = this.slidebarTarget.classList.contains(this.openClass) ? "Close Notifications" : "Open notifications";
+    const isOpen = this.slidebarTarget.classList.contains(this.openClass);
+    const text = isOpen ? "Close Notifications" : "Open notifications";
     this.toggleButtonTarget.textContent = text;
+
+    if (!isOpen) {
+      const userId = this.data.get("userId")
+      window.location.href = `/employees/${userId}`;
+    }
   }
 }
-
